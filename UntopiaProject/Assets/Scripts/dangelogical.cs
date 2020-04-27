85d72c126713bba14b399e8a310f5f72bb44cefa
sergei <seregaposta@gmail.com>
2020-04-27 11:50:58 +0400

    in dange logical
    by vinie

---
+++ UntopiaProject/Assets/Scripts/dangelogical.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class dangelogical : MonoBehaviour
+{
+    public GameObject door;
+    void Start()
+    {
+        door.SetActive(true);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+            door.SetActive(false);
+    }
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        door.SetActive(true);
+    }
+}
